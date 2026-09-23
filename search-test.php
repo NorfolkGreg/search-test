ec10c19eedc3209b3fc359310d0bb46389001890
NorfolkGreg <89905013+NorfolkGreg@users.noreply.github.com>
2026-09-23 16:10:22 +0100

    Update search-test.php

--- search-test.php
+++ search-test.php
@@ -5,5 +5,6 @@ global $Wcms;
 $pages = $Wcms->get('pages');
 $games = $pages->games;
 $table = $games->subpages->table;
+$subpages = $table->subpages;
 
-echo '<!-- Search Test: table fields = ' . implode(', ', array_keys(get_object_vars($table))) . ' -->';
+echo '<!-- Search Test: table subpages type=' . gettype($subpages) . '; properties=' . implode(', ', array_keys(get_object_vars($subpages))) . ' -->';
