6856d5a0aa29d2dbed568238ed33e195a4355899
NorfolkGreg <89905013+NorfolkGreg@users.noreply.github.com>
2026-09-24 19:21:18 +0100

    Update search-test.php

--- search-test.php
+++ search-test.php
@@ -2,8 +2,7 @@
 
 global $Wcms;
 
-function getMenuLinks($items, $parentPath = '') {
-    $links = [];
+function classifyMenuItems($items, $parentPath = '') {
 
     foreach ($items as $item) {
 
@@ -30,56 +29,31 @@ function getMenuLinks($items, $parentPath = '') {
         }
 
         $visibleSubpages = array_filter($subpages, function($subpage) {
-            $subpage = is_array($subpage) ? (object)$subpage : $subpage;
+            $subpage = is_array($subpage)
+                ? (object)$subpage
+                : $subpage;
 
             return !isset($subpage->visibility)
                 || $subpage->visibility !== 'hide';
         });
 
         if (empty($visibleSubpages)) {
-            $links[] = $currentPath;
+            $type = 'LINK';
+        } else {
+            $type = 'BUTTON';
         }
+
+        echo '<p>';
+        echo '<strong>'
+            . htmlspecialchars($currentPath, ENT_QUOTES, 'UTF-8')
+            . '</strong> &mdash; ';
+        echo $type;
+        echo '</p>';
 
         if (!empty($subpages)) {
-            $links = array_merge(
-                $links,
-                getMenuLinks($subpages, $currentPath)
-            );
+            classifyMenuItems($subpages, $currentPath);
         }
     }
-
-    return $links;
-}
-
-function getPageByPath($pages, $path) {
-    $parts = explode('/', $path);
-    $current = $pages;
-
-    foreach ($parts as $index => $part) {
-
-        if ($index === 0) {
-            $current = $current->{$part};
-        } else {
-            $current = $current->subpages->{$part};
-        }
-    }
-
-    return $current;
-}
-
-function cleanSearchTitle($title) {
-
-    /*
-     * Convert HTML entities such as &nbsp; and &#8209;
-     * into their actual characters.
-     */
-    $title = html_entity_decode(
-        $title,
-        ENT_QUOTES | ENT_HTML5,
-        'UTF-8'
-    );
-
-    return $title;
 }
 
 $menuConfig = $Wcms->get('config', 'menuItems');
@@ -88,105 +62,16 @@ $menuItems = is_object($menuConfig)
     ? get_object_vars($menuConfig)
     : (array)$menuConfig;
 
-$menuLinks = getMenuLinks($menuItems);
+echo '<div style="background-color: #fff; padding: 20px;">';
 
-$query = trim($_GET['q'] ?? '');
+echo '<h2>Search Test - Menu Classification</h2>';
 
-$pages = $Wcms->get('pages');
+echo '<p>';
+echo 'LINK = page should be searchable.';
+echo '<br>';
+echo 'BUTTON = container only; page itself should not be searched.';
+echo '</p>';
 
-$matches = [];
-
-foreach ($menuLinks as $path) {
-
-    if ($path === 'search' || $path === '404') {
-        continue;
-    }
-
-    $page = getPageByPath($pages, $path);
-
-    $title = $page->title ?? '';
-    $content = $page->content ?? '';
-
-    $plainContent = html_entity_decode(
-        strip_tags($content),
-        ENT_QUOTES | ENT_HTML5,
-        'UTF-8'
-    );
-
-    $titleMatch = stripos($title, $query) !== false;
-    $contentMatch = stripos($plainContent, $query) !== false;
-
-    if ($titleMatch || $contentMatch) {
-        $matches[] = [
-            'path' => $path,
-            'title' => $title,
-            'titleMatch' => $titleMatch ? 'YES' : 'NO',
-            'contentMatch' => $contentMatch ? 'YES' : 'NO'
-        ];
-    }
-}
-
-echo '<div class="search-test-results" style="background-color: #fff">';
-
-foreach ($matches as $match) {
-
-    $url = '/' . $match['path'];
-
-    $page = getPageByPath($pages, $match['path']);
-    $content = $page->content ?? '';
-
-    $plainContent = html_entity_decode(
-        strip_tags($content),
-        ENT_QUOTES | ENT_HTML5,
-        'UTF-8'
-    );
-
-    $matchPosition = stripos($plainContent, $query);
-
-    if ($matchPosition !== false) {
-        $start = max(0, $matchPosition - 20);
-        $excerpt = substr($plainContent, $start, 100);
-
-        if ($start > 0) {
-            $excerpt = '...' . $excerpt;
-        }
-    } else {
-        $excerpt = substr($plainContent, 0, 100);
-    }
-
-    echo '<p>';
-
-    $cleanTitle = cleanSearchTitle($match['title']);
-
-    echo '<a href="' . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . '">'
-        . htmlspecialchars(
-            $cleanTitle,
-            ENT_QUOTES,
-            'UTF-8'
-        )
-        . '</a><br>';
-
-    $safeExcerpt = htmlspecialchars(
-        $excerpt,
-        ENT_QUOTES,
-        'UTF-8'
-    );
-
-    $safeQuery = htmlspecialchars(
-        $query,
-        ENT_QUOTES,
-        'UTF-8'
-    );
-
-    $highlightedExcerpt = preg_replace(
-        '/(' . preg_quote($safeQuery, '/') . ')/i',
-        '<span style="background-color: yellow">$1</span>',
-        $safeExcerpt
-    );
-
-    echo $highlightedExcerpt;
-
-    echo '</p>';
-}
+classifyMenuItems($menuItems);
 
 echo '</div>';
