95b3308520842521dd02a34a24f2c02a1c32b0dc
NorfolkGreg <89905013+NorfolkGreg@users.noreply.github.com>
2026-09-24 14:02:04 +0100

    Update search-test.php

--- search-test.php
+++ search-test.php
@@ -2,55 +2,6 @@
 
 global $Wcms;
 
-function getMenuLinks($items, $parentPath = '') {
-    $links = [];
-
-    foreach ($items as $item) {
-
-        if (is_array($item)) {
-            $item = (object)$item;
-        }
-
-        $slug = trim($item->slug ?? '', '/');
-
-        if ($slug === '') {
-            continue;
-        }
-
-        $currentPath = $parentPath === ''
-            ? $slug
-            : $parentPath . '/' . $slug;
-
-        $subpages = [];
-
-        if (!empty($item->subpages)) {
-            $subpages = is_object($item->subpages)
-                ? get_object_vars($item->subpages)
-                : (array)$item->subpages;
-        }
-
-        $visibleSubpages = array_filter($subpages, function($subpage) {
-            $subpage = is_array($subpage) ? (object)$subpage : $subpage;
-
-            return !isset($subpage->visibility)
-                || $subpage->visibility !== 'hide';
-        });
-
-        if (empty($visibleSubpages)) {
-            $links[] = $currentPath;
-        }
-
-        if (!empty($subpages)) {
-            $links = array_merge(
-                $links,
-                getMenuLinks($subpages, $currentPath)
-            );
-        }
-    }
-
-    return $links;
-}
-
 function getPageByPath($pages, $path) {
     $parts = explode('/', $path);
     $current = $pages;
@@ -67,109 +18,51 @@ function getPageByPath($pages, $path) {
     return $current;
 }
 
-$menuConfig = $Wcms->get('config', 'menuItems');
-
-$menuItems = is_object($menuConfig)
-    ? get_object_vars($menuConfig)
-    : (array)$menuConfig;
-
-$menuLinks = getMenuLinks($menuItems);
-
-$query = 'wo';
-
 $pages = $Wcms->get('pages');
 
-$matches = [];
+$testPaths = [
+    'wondercms/web-design',
+    'wondercms/themes/gregcustom/media',
+    'wondercms/editors/external/windows/notepad2',
+    'sundry/opencamera',
+    'sundry/wintertonwalk'
+];
 
-foreach ($menuLinks as $path) {
+echo '<div style="background-color: #fff; padding: 20px;">';
 
-    if ($path === 'search' || $path === '404') {
-        continue;
-    }
+echo '<h2>Search Test - Title Diagnostic</h2>';
+
+foreach ($testPaths as $path) {
 
     $page = getPageByPath($pages, $path);
 
     $title = $page->title ?? '';
-    $content = $page->content ?? '';
 
-    $plainContent = html_entity_decode(
-        strip_tags($content),
-        ENT_QUOTES | ENT_HTML5,
-        'UTF-8'
-    );
+    echo '<h3>' . htmlspecialchars($path, ENT_QUOTES, 'UTF-8') . '</h3>';
 
-    $titleMatch = stripos($title, $query) !== false;
-    $contentMatch = stripos($plainContent, $query) !== false;
+    echo '<p><strong>Title as PHP sees it:</strong><br>';
 
-    if ($titleMatch || $contentMatch) {
-        $matches[] = [
-            'path' => $path,
-            'title' => $title,
-            'titleMatch' => $titleMatch ? 'YES' : 'NO',
-            'contentMatch' => $contentMatch ? 'YES' : 'NO'
-        ];
-    }
-}
-
-echo '<div class="search-test-results" style="background-color: #fff">';
-
-foreach ($matches as $match) {
-
-    $url = '/' . $match['path'];
-
-    $page = getPageByPath($pages, $match['path']);
-    $content = $page->content ?? '';
-
-    $plainContent = html_entity_decode(
-        strip_tags($content),
-        ENT_QUOTES | ENT_HTML5,
-        'UTF-8'
-    );
-
-    $matchPosition = stripos($plainContent, $query);
-
-    if ($matchPosition !== false) {
-        $start = max(0, $matchPosition - 20);
-        $excerpt = substr($plainContent, $start, 100);
-
-        if ($start > 0) {
-            $excerpt = '...' . $excerpt;
-        }
-    } else {
-        $excerpt = substr($plainContent, 0, 100);
-    }
-
-    echo '<p>';
-
-    echo '<a href="' . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . '">'
-        . htmlspecialchars(
-            $match['title'],
-            ENT_QUOTES,
-            'UTF-8'
-        )
-        . '</a><br>';
-
-    $safeExcerpt = htmlspecialchars(
-        $excerpt,
+    echo htmlspecialchars(
+        $title,
         ENT_QUOTES,
         'UTF-8'
     );
-
-    $safeQuery = htmlspecialchars(
-        $query,
-        ENT_QUOTES,
-        'UTF-8'
-    );
-
-    $highlightedExcerpt = preg_replace(
-        '/(' . preg_quote($safeQuery, '/') . ')/i',
-        '<span style="background-color: yellow">$1</span>',
-        $safeExcerpt
-    );
-
-    echo $highlightedExcerpt;
 
     echo '</p>';
+
+    echo '<p><strong>Hexadecimal bytes:</strong><br>';
+
+    echo '<code>';
+
+    echo htmlspecialchars(
+        bin2hex($title),
+        ENT_QUOTES,
+        'UTF-8'
+    );
+
+    echo '</code></p>';
+
+    echo '<hr>';
 }
 
 echo '</div>';
